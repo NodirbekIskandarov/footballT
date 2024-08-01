6b095c5fb0c992e58cddd69e81731763027c5ba1
Искандаров Н. Р. <n.iskandarov@stat.local>
2024-08-01 14:19:55 +0500

    created sidebar

--- src/pages/MainLayout/index.jsx
+++ src/pages/MainLayout/index.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Wrapper } from "../../global_styles/styles";
 import { Footer, Header } from "../../components";
+import Sidebar from "../../components/sidebar";
 function MainLayout() {
     return (
         <Wrapper height="100vh">
-            <Header />
+            {/* <Header />
             <Outlet />
-            <Footer />
+            <Footer /> */}
+            <Sidebar/>
         </Wrapper>
     )
 }
