e9c8116f6a508347b8253a62c923ffbebefa6e0a
Искандаров Н. Р. <n.iskandarov@stat.local>
2024-08-01 14:26:10 +0500

    sidebar exported corrected

--- src/pages/MainLayout/index.jsx
+++ src/pages/MainLayout/index.jsx
@@ -1,13 +1,8 @@
-import { Outlet } from "react-router-dom";
-import { Wrapper } from "../../global_styles/styles";
-import { Footer, Header } from "../../components";
 import Sidebar from "../../components/sidebar";
+import { Wrapper } from "../../global_styles/styles";
 function MainLayout() {
     return (
         <Wrapper height="100vh">
-            {/* <Header />
-            <Outlet />
-            <Footer /> */}
             <Sidebar/>
         </Wrapper>
     )
