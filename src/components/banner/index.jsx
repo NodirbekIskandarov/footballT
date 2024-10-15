d1e466be631d39a258bdf943f595ee082c7a96b2
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-10-15 15:08:23 +0500

    banner carusel uchun start yaratildi

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,10 +1,42 @@
-import styles from "./banner.module.scss";
-function Banner() {
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+// Import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import img1 from '../../assets/images/banner.png'; // Replace with your actual image path
+
+export default function Banner() {
   return (
-    <div className={styles.banner}>
-      banner
-    </div>
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={img1} alt="Slide 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img1} alt="Slide 2" />
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 }
-
-export default Banner;
