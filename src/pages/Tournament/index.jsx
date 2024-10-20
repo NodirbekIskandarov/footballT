8cc5912da18d263ee8383f28e8151e3260b3a759
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-10-20 19:04:35 +0500

    .

--- src/pages/Tournament/index.jsx
+++ src/pages/Tournament/index.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { TournamentBanner } from "../../components";
 import styles from "./style.module.scss";
 export default function Tournament() {
@@ -6,8 +6,8 @@ export default function Tournament() {
     <div>
       <TournamentBanner />
       <div className={styles.container}>
-        <div className={styles.left}>Otkazilgan turnirlar</div>
-        <div className={styles.right}>Rejalashtirilgan turnirlar</div>
+        <div className={styles.left}><Link className={styles.link} to="pasted">Otkazilgan turnirlar</Link></div>
+        <div className={styles.right}><Link className={styles.link} to="planed">Rejalashtirilgan turnirlar</Link></div>
       </div>
 
       <Outlet />
