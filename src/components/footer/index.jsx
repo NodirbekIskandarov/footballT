49d1bd5ad46b8baa22bc5fbf3e98f26a4758f1f7
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-10-20 15:29:23 +0500

    footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,43 @@
-
+import styles from './footer.module.scss'
+import logotip from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom'
+import instagram from '../../assets/images/instagram.png'
+import telegram from '../../assets/images/telegram.png'
+import facebook from '../../assets/images/Facebook.png'
 function Footer() {
   return (
-    <div>
-        footer
+    <div className={styles.footer}>
+      <div className="container">
+        <div className={styles.footer_child}>
+            <div className={styles.logo_part}>
+                <div className={styles.line_}></div>
+                <div className={styles.logotip}><img src={logotip} alt="logotip" /></div>
+                <div className={styles.line__}></div>
+            </div>
+            <div className={styles.menus}>
+                <Link className={styles.link} to='/'>Home</Link>
+                <Link className={styles.link} to='/'>Tournaments </Link>
+                <Link className={styles.link} to='/'>News</Link>
+                <Link className={styles.link} to='/'>Media</Link>
+                <Link className={styles.link} to='/'>History</Link>
+                <Link className={styles.link} to='/'>About us</Link>
+            </div>
+            <div className={styles.socials}>
+              <div className={styles.social}>
+                <img src={instagram} alt="instagram" />
+              </div>
+              <div className={styles.social}>
+                <img src={telegram} alt="instagram" />
+              </div>
+              <div className={styles.social}>
+                <img src={facebook} alt="instagram" />
+              </div>
+            </div>
+            <div className={styles.text}>
+              <span>Super Leguage 2024</span>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
