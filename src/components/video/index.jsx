018d8f376da7d565d459b4fcbc3444d083b7a024
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-10-21 17:04:13 +0500

    .

--- src/components/video/index.jsx
+++ src/components/video/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from './style.module.scss'
+import mediaphoto from '../../assets/images/mediaphoto.png'
 function Video() {
   return (
-    <div>Video</div>
+    <div className={styles.video}>
+      <div className='container'>
+      <div className={styles.boxes}>
+          <div className={styles.box}>
+            <img src={mediaphoto} alt="media photo" />
+            <div className={styles.text}>
+              <span>Uzbekistan kubogi 2024</span>
+              <span>14 June</span>
+              <span>Final OIL STAR vs  BMB</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
