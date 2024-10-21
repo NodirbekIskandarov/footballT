83b1c76606027aba65a3563da48998f5b3d0d4c6
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-10-21 16:39:17 +0500

    photo

--- src/components/photo/index.jsx
+++ src/components/photo/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import styles from './style.module.scss'
+import mediaphoto from '../../assets/images/mediaphoto.png'
 function Photo() {
   return (
-    <div>Photo</div>
+    <div className={styles.photo}>
+      <div className='container'>
+        <div className={styles.boxes}>
+          <div className={styles.box}>
+            <img src={mediaphoto} alt="media photo" />
+            <div className={styles.text}>
+              <span>Uzbekistan kubogi 2024</span>
+              <span>14 June</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
