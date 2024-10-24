c5e71f0127613aa0c087df4c89273ab20fdd1acc
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-10-24 15:29:05 +0500

    best player page

--- src/pages/BestPlayers/index.jsx
+++ src/pages/BestPlayers/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './style.module.scss'
+import player from '../../assets/images/player.png'
+import { FaArrowRightLong } from "react-icons/fa6"
 function BestPlayers() {
   return (
     <div className={styles.players}>
@@ -10,6 +12,15 @@ function BestPlayers() {
                     <option value="">2024</option>
                     <option value="">2024</option>
                 </select>
+            </div>
+
+            <div className={styles.boxes}>
+                <div className={styles.box}>
+                    <img src={player} alt="player" />
+                    <span className={styles.name}>Asadov Asadbek</span>
+                    <span className={styles.position}>O’yinchi</span>
+                    <button>View <FaArrowRightLong/></button>
+                </div>
             </div>
         </div>
     </div>
