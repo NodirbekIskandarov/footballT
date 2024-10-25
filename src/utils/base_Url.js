ce7de7e3293055fb21ff39e894f7ae29d9b1bbbc
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-10-25 20:04:07 +0500

    created request function

---
+++ src/utils/base_Url.js
@@ -0,0 +1,27 @@
+ // Import the axios instance
+
+ import instance from "./base_Url";
+
+ export const getRequest = (url) => {
+     return new Promise((resolve, reject) => {
+         instance.get(url)  // Use instance instead of axios.get
+         .then(response => {
+             resolve(response);
+         })
+         .catch(error => {
+             reject(error);
+         });
+     });
+ };
+ 
+ export const postRequest = (url, data) => {
+     return new Promise((resolve, reject) => {
+         instance.post(url, data)  // Use instance instead of axios.post
+         .then((response) => {
+             resolve(response);
+         })
+         .catch((error) => {
+             reject(error);
+         });
+     });
+ };
