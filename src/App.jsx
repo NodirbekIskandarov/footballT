ff837d8e902462ec08f1d0b7d9d19806464d214c
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-10-30 19:34:40 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { AboutPlayer, AboutUs, BannerDetail, BestPlayers, Events, Home, Kids, Live, MainLayout, Media, News, PastedGames, PlanedGames, Players, Preview, PreviewC, Protocol, Tounament } from './pages'
+import { AboutPlayer, AboutUs, BannerDetail, BestPlayers, Events, Home, Kids, MainLayout, Media, News, PastedGames, PlanedGames, Players, Preview, PreviewC, Protocol, Tounament } from './pages'
 import './App.css'
 import { Photo, Video } from './components'
 import './i18n'
