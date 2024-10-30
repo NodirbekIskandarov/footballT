456036c4371f7a9da56f68fd01dafe4c8df8e6dd
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-10-30 22:51:07 +0500

    .

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,17 +4,33 @@ import { Link } from 'react-router-dom';
 import instagram from '../../assets/images/instagram.png';
 import telegram from '../../assets/images/telegram.png';
 import facebook from '../../assets/images/facebook.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import { getRequest } from '../../utils/request';
+import { tournament_list } from '../../utils/API_urls';
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 function Footer() {
   const { t } = useTranslation();
   const [submenuOpen, setSubmenuOpen] = useState(false);
-
+  const [data, setData] = useState(null);
   const toggleSubmenu = () => {
     setSubmenuOpen(!submenuOpen);
   };
 
+
+  const handleClick = () => {
+    setSubmenuOpen(false); // Close submenu when a link is clicked
+  };
+  useEffect(() => {
+    getRequest(tournament_list)
+      .then((response) => {
+        setData(response?.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className={styles.footer}>
       <div className="container">
@@ -29,12 +45,24 @@ function Footer() {
           <div className={styles.menus}>
             <Link className={styles.link} to="/">{t("Home")}</Link>
             <div className={styles.tournament} onClick={toggleSubmenu}>
-              <span className={styles.link}>{t("Tournaments")}</span>
+              <span className={styles.link}>
+                {t("Tournaments")}{" "}
+                {submenuOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+              </span>
               {submenuOpen && (
                 <div className={styles.submenu}>
-                  <Link className={styles.submenu_link} to="/tournament/upcoming">Upcoming</Link>
-                  <Link className={styles.submenu_link} to="/tournament/past">Past Tournaments</Link>
-                  <Link className={styles.submenu_link} to="/tournament/rules">Tournament Rules</Link>
+                  {data?.map((item, index) => {
+                    return (
+                      <Link
+                        className={styles.submenu_link}
+                        to={`/tournament/pasted/${item?.uuid}`}
+                        onClick={handleClick}
+                        key={index}
+                      >
+                        {item?.name}
+                      </Link>
+                    );
+                  })}
                 </div>
               )}
             </div>
