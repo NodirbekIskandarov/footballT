f2f51175512ca6db2bb0ad29c08b6768756037b9
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-10-30 14:03:57 +0500

    .

--- src/pages/Live/index.jsx
+++ src/pages/Live/index.jsx
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from 'react'
 import styles from './style.module.scss'
-import { useTranslation } from 'react-i18next';
-import { getRequest } from '../../utils/request';
-import { match_detail } from '../../utils/API_urls';
-import { useParams } from 'react-router-dom';
 
 function Live() {
-  const {t} = useTranslation()
-  const [data, setData] = useState(null);
-  const pk = useParams();
-  useEffect(() => {
-    getRequest(`${match_detail}${pk.id}`)
-      .then((response) => {
-        // console.log(response?.data, "live");
-        setData(response?.data);
-        
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [pk.id]);
+  
   return (
     <div className={styles.live}>
       <div className="container">
-        <div className={styles.box}>
+        {/* <div className={styles.box}>
           <div className={styles.clubs}>
             <div className={styles.club}>
               <img src={data?.team1?.icon_url} alt="" />
@@ -36,7 +18,7 @@ function Live() {
           </div>
           <div className={styles.card}>
           <a
-              href={data?.live_link} // Dynamic link for each video
+              href={data?.live_link} 
               target="_blank"
               rel="noopener noreferrer"
               key={1}
@@ -44,7 +26,7 @@ function Live() {
             >
               <div className={styles.box}>
                 <img src='' alt="media photo" />
-                <b className={styles.playicon}>▶</b> {/* Play icon */}
+                <b className={styles.playicon}>▶</b> 
                 <div className={styles.text}>
                   <span>;;;</span>
                   <span>ksjdb</span>
@@ -52,7 +34,7 @@ function Live() {
               </div>
             </a>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
