02c484b6d9c96ad0bde7d2a4fff6c29cd1051b66
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-11-01 20:33:15 +0500

    .socails ulandi

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,23 +1,21 @@
-import styles from './footer.module.scss';
-import logotip from '../../assets/images/logo.png';
-import { Link } from 'react-router-dom';
-import instagram from '../../assets/images/instagram.png';
-import telegram from '../../assets/images/telegram.png';
-import facebook from '../../assets/images/facebook.png';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { getRequest } from '../../utils/request';
-import { tournament_list } from '../../utils/API_urls';
+import styles from "./footer.module.scss";
+import logotip from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { getRequest } from "../../utils/request";
+import { socials, tournament_list } from "../../utils/API_urls";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 function Footer() {
   const { t } = useTranslation();
   const [submenuOpen, setSubmenuOpen] = useState(false);
   const [data, setData] = useState(null);
+  const [socialss, setSocialss] = useState(null);
+
   const toggleSubmenu = () => {
     setSubmenuOpen(!submenuOpen);
   };
-
 
   const handleClick = () => {
     setSubmenuOpen(false); // Close submenu when a link is clicked
@@ -31,6 +29,17 @@ function Footer() {
         console.log(error);
       });
   }, []);
+
+  useEffect(() => {
+    getRequest(socials)
+      .then((response) => {
+        setSocialss(response?.data);
+        console.log(response?.data, "socials");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className={styles.footer}>
       <div className="container">
@@ -38,12 +47,16 @@ function Footer() {
           <div className={styles.logo_part}>
             <div className={styles.line_}></div>
             <div className={styles.logotip}>
-              <a href="/"><img src={logotip} alt="logo" /></a>
+              <a href="/">
+                <img src={logotip} alt="logo" />
+              </a>
             </div>
             <div className={styles.line__}></div>
           </div>
           <div className={styles.menus}>
-            <Link className={styles.link} to="/">{t("Home")}</Link>
+            <Link className={styles.link} to="/">
+              {t("Home")}
+            </Link>
             <div className={styles.tournament} onClick={toggleSubmenu}>
               <span className={styles.link}>
                 {t("Tournaments")}{" "}
@@ -66,21 +79,33 @@ function Footer() {
                 </div>
               )}
             </div>
-            <Link className={styles.link} to="/news">{t("News")}</Link>
-            <Link className={styles.link} to="/media">{t("Media")}</Link>
-            <Link className={styles.link} to="/best-players">{t("History")}</Link>
-            <Link className={styles.link} to="/about-us">{t("About us")}</Link>
+            <Link className={styles.link} to="/news">
+              {t("News")}
+            </Link>
+            <Link className={styles.link} to="/media">
+              {t("Media")}
+            </Link>
+            <Link className={styles.link} to="/best-players">
+              {t("History")}
+            </Link>
+            <Link className={styles.link} to="/about-us">
+              {t("About us")}
+            </Link>
           </div>
           <div className={styles.socials}>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-              <img src={instagram} alt="Instagram" />
-            </a>
-            <a href="https://telegram.org" target="_blank" rel="noopener noreferrer">
-              <img src={telegram} alt="Telegram" />
-            </a>
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-              <img src={facebook} alt="Facebook" />
-            </a>
+            {socialss?.map((item, index) => {
+              return (
+                <div className={styles.social} key={index}>
+                  <a
+                    href={item?.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={item?.icon} alt={item?.name} />
+                  </a>
+                </div>
+              );
+            })}
           </div>
           <div className={styles.text}>
             <span>Super Leguage 2024</span>
