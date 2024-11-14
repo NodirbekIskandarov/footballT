105dfe5728016cb02ebcbc31bfc59d68494b2e42
Nodirbek <nodirbekiskandarov2003@gmail.com>
2024-11-14 16:26:08 +0500

    .

--- src/pages/pastedGames/index.jsx
+++ src/pages/pastedGames/index.jsx
@@ -5,21 +5,6 @@ import ResultsTournamentTable from "../../components/resultsTournamentTable"
 function PastedGames() {
   return (
     <div>
-      {/* <div className="container">
-        <div style={{
-          marginTop: "50px",
-          display: "flex",
-          gap: "15px"
-        }}>
-          <span style={{
-            fontWeight: "500",
-            fontSize: "28px"
-          }}>Filter</span>
-          <input type="text"  placeholder="kun"/>
-          <input type="text"  placeholder="oy"/>
-          <input type="text"  placeholder="yil"/>
-        </div>
-      </div> */}
       <GamesTournament/>
       <ResultsTournamentTable/>
       <PlayersTournament/>
