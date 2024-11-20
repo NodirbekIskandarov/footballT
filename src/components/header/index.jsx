6e374a903d7ba09d5513a815e8f86b200fd309de
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-11-20 19:56:20 +0500

    subsub menu list added

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,3 @@
-
-
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.png";
 import styles from "./header.module.scss";
@@ -8,13 +6,15 @@ import { useEffect, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 import { getRequest } from "../../utils/request";
-import { tournament_list } from "../../utils/API_urls";
+import { sub_tournament_list, tournament_list } from "../../utils/API_urls";
 import { useTranslation } from "react-i18next";
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [submenuOpen, setSubmenuOpen] = useState(false);
+  const [subsubmenuOpen, setSubsubmenuOpen] = useState(false);
   const [data, setData] = useState(null);
+  const [subsubmenulist, setSubsubmenulist] = useState([]);
   const { t, i18n } = useTranslation();
   const [activeMenu, setActiveMenu] = useState(""); // State to track active menu item
 
@@ -24,13 +24,29 @@ function Header() {
   };
 
   const toggleSubmenu = () => {
-    setSubmenuOpen(!submenuOpen);
+    setSubmenuOpen(true);
   };
 
   const handleClick = (menu) => {
     setActiveMenu(menu); // Set the active menu item
     setMenuOpen(false);
     setSubmenuOpen(false);
+  };
+
+  const handleMenuClick = (id) => {
+    getRequest(sub_tournament_list + id)
+      .then((response) => {
+        if (response?.data.length !== 0) {
+          setSubsubmenulist(response?.data);
+          setSubsubmenuOpen(true);
+        } else {
+          setSubsubmenulist([]);
+          setSubsubmenuOpen(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   useEffect(() => {
@@ -54,59 +70,118 @@ function Header() {
       <div className="container">
         <div className={styles.header_main}>
           <div className={styles.img_part}>
-            <a href="/"><img src={logo} alt="logo" /></a>
+            <a href="/">
+              <img src={logo} alt="logo" />
+            </a>
           </div>
           <div className={styles.father}>
-          <FaBars className={styles.hamburger} onClick={toggleMenu} />
-            <div className={`${styles.menu_part} ${menuOpen ? styles.menu_open : ""}`}>
+            <FaBars className={styles.hamburger} onClick={toggleMenu} />
+            <div
+              className={`${styles.menu_part} ${
+                menuOpen ? styles.menu_open : ""
+              }`}
+            >
               <FaTimes className={styles.close_icon} onClick={toggleMenu} />
-              <Link className={`${styles.link} ${activeMenu === "home" ? styles.active : ""}`} to="/" onClick={() => handleClick("home")}>
+              <Link
+                className={`${styles.link} ${
+                  activeMenu === "home" ? styles.active : ""
+                }`}
+                to="/"
+                onClick={() => handleClick("home")}
+              >
                 {t("Home")}
               </Link>
               <div className={styles.tournament} onClick={toggleSubmenu}>
-                <span className={`${styles.link} ${activeMenu === "tournaments" ? styles.active : ""}`}>
+                <span
+                  className={`${styles.link} ${
+                    activeMenu === "tournaments" ? styles.active : ""
+                  }`}
+                >
                   {t("Tournaments")}{" "}
                   {submenuOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
                 </span>
                 {submenuOpen && (
                   <div className={styles.submenu}>
                     {data?.map((item) => (
-                      <Link
+                      <span
                         className={styles.submenu_link}
-                        to={`/tournament/pasted/${item?.uuid}`}
-                        onClick={() => handleClick(item.name)}
+                        onClick={() => {
+                          handleClick(item.name);
+                          handleMenuClick(item.uuid);
+                        }}
                         key={item.uuid}
                       >
                         {item[`name_${i18n.language}`]}
-                      </Link>
+                      </span>
                     ))}
                   </div>
                 )}
+                {subsubmenuOpen && (
+                  <div className={styles.subsubmenu}>
+                    {subsubmenulist?.map((item) => {
+                      return (
+                        <Link
+                          to={`/tournament/pasted/${item?.uuid}`}
+                          className={styles.subsubmenu_link}
+                          onClick={() => handleClick(item.name)}
+                          key={item.uuid}
+                        >
+                          {item[`name_${i18n.language}`]}
+                        </Link>
+                      );
+                    })}
+                  </div>
+                )}
               </div>
-              <Link className={`${styles.link} ${activeMenu === "news" ? styles.active : ""}`} to="/news" onClick={() => handleClick("news")}>
+              <Link
+                className={`${styles.link} ${
+                  activeMenu === "news" ? styles.active : ""
+                }`}
+                to="/news"
+                onClick={() => handleClick("news")}
+              >
                 {t("News")}
               </Link>
-              <Link className={`${styles.link} ${activeMenu === "media" ? styles.active : ""}`} to="/media" onClick={() => handleClick("media")}>
+              <Link
+                className={`${styles.link} ${
+                  activeMenu === "media" ? styles.active : ""
+                }`}
+                to="/media"
+                onClick={() => handleClick("media")}
+              >
                 {t("Media")}
               </Link>
-              <Link className={`${styles.link} ${activeMenu === "best-players" ? styles.active : ""}`} to="/best-players" onClick={() => handleClick("best-players")}>
+              <Link
+                className={`${styles.link} ${
+                  activeMenu === "best-players" ? styles.active : ""
+                }`}
+                to="/best-players"
+                onClick={() => handleClick("best-players")}
+              >
                 {t("History")}
               </Link>
-              <Link className={`${styles.link} ${activeMenu === "about-us" ? styles.active : ""}`} to="/about-us" onClick={() => handleClick("about-us")}>
+              <Link
+                className={`${styles.link} ${
+                  activeMenu === "about-us" ? styles.active : ""
+                }`}
+                to="/about-us"
+                onClick={() => handleClick("about-us")}
+              >
                 {t("About us")}
               </Link>
             </div>
 
             <div className={styles.language_part}>
-              <select defaultValue={i18n.language} onChange={handleLanguageChange}>
+              <select
+                defaultValue={i18n.language}
+                onChange={handleLanguageChange}
+              >
                 <option value="uz">UZ</option>
                 <option value="ru">RU</option>
                 <option value="en">EN</option>
               </select>
             </div>
           </div>
-          
-
         </div>
       </div>
     </div>
