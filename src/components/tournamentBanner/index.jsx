6c2893aef07e191e1de6a080cbb2d51a61703f32
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-11-24 16:24:58 +0500

    .

--- src/components/tournamentBanner/index.jsx
+++ src/components/tournamentBanner/index.jsx
@@ -1,9 +1,25 @@
 import styles from './style.module.scss'
 import bannerImg from '../../assets/images/tournamentbanner.png'
+import { useEffect, useState } from 'react'
+import { getRequest } from '../../utils/request'
+import { useParams } from 'react-router-dom'
+import { get_league_banner } from '../../utils/API_urls'
 function TournamentBanner() {
+  const {id} = useParams()
+  const [data, setData] = useState(null)
+  useEffect(() => {
+    getRequest(get_league_banner+id)
+    .then(response => {
+      console.log(response)
+      setData(response?.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [id])
   return (
     <div className={styles.banner}>
-        <img src={bannerImg} alt="tournament banner" />
+        <img src={data?.icon ?? bannerImg} alt="tournament banner" />
     </div>
   )
 }
