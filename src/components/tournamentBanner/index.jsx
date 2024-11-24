2a746ed601979c039e88e7e94b6026f585fe3517
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-11-24 17:33:32 +0500

    .

--- src/components/tournamentBanner/index.jsx
+++ src/components/tournamentBanner/index.jsx
@@ -1,26 +1,27 @@
-import styles from './style.module.scss'
-import { useEffect, useState } from 'react'
-import { getRequest } from '../../utils/request'
-import { useParams } from 'react-router-dom'
-import { get_league_banner } from '../../utils/API_urls'
+import styles from "./style.module.scss";
+import { useEffect, useState } from "react";
+import { getRequest } from "../../utils/request";
+import { useParams } from "react-router-dom";
+import { get_league_banner } from "../../utils/API_urls";
 function TournamentBanner() {
-  const {id} = useParams()
-  const [data, setData] = useState(null)
+  const { id } = useParams();
+  const [data, setData] = useState(null);
   useEffect(() => {
-    getRequest(get_league_banner+id)
-    .then(response => {
-      console.log(response)
-      setData(response?.data)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }, [id])
+    getRequest(get_league_banner + id)
+      .then((response) => {
+        console.log(response);
+        setData(response?.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
   return (
     <div className={styles.banner}>
-        <img src={data?.icon} alt="tournament banner" />
+      <img src={data?.icon} alt="tournament banner" />
+      <h1 style={{ margin: "30px 0" }}>{data?.name}</h1>
     </div>
-  )
+  );
 }
 
-export default TournamentBanner
+export default TournamentBanner;
