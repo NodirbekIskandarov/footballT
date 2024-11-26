c74f2ae3e9a9a37b47e867237bf7ddc7f66c6e31
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-11-26 19:43:50 +0500

    home banner da o'zgarish

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -20,8 +20,7 @@ import { Tooltip } from "@mui/material";
 
 export default function Banner() {
   const [data, setData] = useState([]);
-  const { t, i18n } = useTranslation();
-  const lng = i18n.language;
+  const { t } = useTranslation();
 
   useEffect(() => {
     getRequest(home_banner)
@@ -53,7 +52,71 @@ export default function Banner() {
           data?.map((item, index) => {
             return (
               <SwiperSlide key={index}>
-                <Tooltip followCursor title={t("Ko'proq ma'lumot uchun rasm ustiga bosing")}>
+                {item?.match?.is_active ? (
+                  <Tooltip
+                    followCursor
+                    title={t("Ko'proq ma'lumot uchun rasm ustiga bosing")}
+                  >
+                    <div
+                      style={{
+                        width: "100%",
+                        height: "100%", // CSS fayli orqali boshqariladi
+                        backgroundImage: `url(${item?.photo})`,
+                        backgroundPosition: "center",
+                        backgroundSize: "cover",
+                        backgroundRepeat: "no-repeat",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <Link
+                        to={item?.uuid}
+                        style={{ width: "100%", height: "100%" }}
+                      ></Link>
+                      {/* <div
+                      style={{
+                        width: "90%", // Mobil va tablet uchun kenglikni qisqartirish
+                        maxWidth: "800px",
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "10px",
+                        padding: "20px",
+                        backgroundColor: "rgba(0, 0, 0, 0.5)",
+                        backdropFilter: "blur(5px)",
+                        borderRadius: "10px"
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontWeight: "500",
+                          fontSize: "32px", // Desktop: 48px, Tablet: 32px, Mobil: 24px
+                          color: "#FFFFFF",
+                          textAlign: "center", // Mobil va tabletda matnni markazlashtirish
+                        }}
+                      >
+                        {item[`title_${lng}`]}
+                      </span>
+                      <Link to={item?.uuid}>
+                        <button
+                          style={{
+                            padding: "12px 16px",
+                            backgroundColor: "#FFFFFF",
+                            borderRadius: "8px",
+                            border: "none",
+                            fontSize: "18px",
+                            color: "#1E1E1E",
+                            cursor: "pointer",
+                            alignSelf: "center", // Mobil va tabletda markaziy joylashuv
+                          }}
+                        >
+                          {t("Ko’proq ma’lumot")}
+                        </button>
+                      </Link>
+                    </div> */}
+                    </div>
+                  </Tooltip>
+                ) : (
                   <div
                     style={{
                       width: "100%",
@@ -67,7 +130,6 @@ export default function Banner() {
                       alignItems: "center",
                     }}
                   >
-                    <Link to={item?.uuid } style={{width: "100%", height: "100%"}}></Link>
                     {/* <div
                       style={{
                         width: "90%", // Mobil va tablet uchun kenglikni qisqartirish
@@ -109,7 +171,7 @@ export default function Banner() {
                       </Link>
                     </div> */}
                   </div>
-                </Tooltip>
+                )}
               </SwiperSlide>
             );
           })}
