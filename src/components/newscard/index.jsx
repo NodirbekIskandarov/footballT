0c8e39a69b197faa09f8734659ae7db5f5f9a73a
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-11-26 20:05:49 +0500

    .

--- src/components/newscard/index.jsx
+++ src/components/newscard/index.jsx
@@ -2,16 +2,16 @@ import styles from "./style.module.scss";
 import { useEffect, useState } from "react";
 import { getRequest } from "../../utils/request";
 import { all_news } from "../../utils/API_urls";
-import 'aos/dist/aos.css'; // AOS CSS import
-import AOS from 'aos'; // AOS import
+import "aos/dist/aos.css"; // AOS CSS import
+import AOS from "aos"; // AOS import
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
-import clublogo from '../../assets/images/club-logo2.jpg'
+import clublogo from "../../assets/images/club-logo2.jpg";
 function NewsCard() {
   const [data, setData] = useState(null);
-  const { i18n } = useTranslation()
-  const lng = i18n.language
-  const navigate = useNavigate()
+  const { i18n } = useTranslation();
+  const lng = i18n.language;
+  const navigate = useNavigate();
 
   useEffect(() => {
     getRequest(all_news)
@@ -30,29 +30,49 @@ function NewsCard() {
     });
   }, []);
 
-  function navigateFunc(id) {
-    navigate(`/${id}`)
+  function navigateFunc(item) {
+    if (item?.match?.is_active) {
+      navigate(`/${item?.uuid}`);
+    }
   }
 
   return (
-    <div className={styles.news_card} data-aos="fade-left"> {/* Yangi animatsiya */}
+    <div className={styles.news_card} data-aos="fade-left">
+      {" "}
+      {/* Yangi animatsiya */}
       <div className="container">
         {data?.map((item, index) => {
           return (
-            <div className={styles.card} key={index} data-aos="fade-left" onClick={() => navigateFunc(item?.uuid)} style={{ cursor: "pointer" }}> {/* Yangi animatsiya */}
+            <div
+              className={styles.card}
+              key={index}
+              data-aos="fade-left"
+              onClick={() => navigateFunc(item)}
+              style={{ cursor: "pointer" }}
+            >
+              {" "}
+              {/* Yangi animatsiya */}
               <div className={styles.left}>
-                <div className={styles.club_part}>
-                  <img src={item?.match?.team1?.icon_url ?? clublogo} alt="club logotip" />
-                  {/* <span>{item?.match?.team1[`name_${lng}`]}</span> */}
-                </div>
-                <span className={styles.vs}>vs</span>
-                <div className={styles.club_part}>
-                  <img src={item?.match?.team2?.icon_url ?? clublogo} alt="club logotip" />
-                  {/* <span>{item?.match?.team2[`name_${lng}`]}</span> */}
-                </div>
-                <span className={styles.text}>
-                  {item[`title_${lng}`]}
-                </span>
+                {item?.match?.is_active && (
+                  <>
+                    <div className={styles.club_part}>
+                      <img
+                        src={item?.match?.team1?.icon_url ?? clublogo}
+                        alt="club logotip"
+                      />
+                      {/* <span>{item?.match?.team1[`name_${lng}`]}</span> */}
+                    </div>
+                    <span className={styles.vs}>vs</span>
+                    <div className={styles.club_part}>
+                      <img
+                        src={item?.match?.team2?.icon_url ?? clublogo}
+                        alt="club logotip"
+                      />
+                      {/* <span>{item?.match?.team2[`name_${lng}`]}</span> */}
+                    </div>
+                  </>
+                )}
+                <span className={styles.text}>{item[`title_${lng}`]}</span>
               </div>
               <div className={styles.right}>
                 <img src={item?.photo} alt="team photos" />
