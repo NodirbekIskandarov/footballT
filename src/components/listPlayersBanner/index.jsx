109e1c1be6e70ab7fb0f12f28267e322db66ddb5
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-12-23 18:49:27 +0500

    .

--- src/components/listPlayersBanner/index.jsx
+++ src/components/listPlayersBanner/index.jsx
@@ -1,27 +1,28 @@
-import { useEffect, useState } from 'react';
-import styles from './style.module.scss';
-import { Link, useParams } from 'react-router-dom';
-import 'aos/dist/aos.css'; // AOS CSS import
-import AOS from 'aos'; // AOS import
-import { getRequest } from '../../utils/request';
-import { playerbyteam } from '../../utils/API_urls';
-import { useTranslation } from 'react-i18next';
-import clublogo from '../../assets/images/club-logo2.jpg'
+import { useEffect, useState } from "react";
+import styles from "./style.module.scss";
+import { useParams } from "react-router-dom";
+import "aos/dist/aos.css"; // AOS CSS import
+import AOS from "aos"; // AOS import
+import { getRequest } from "../../utils/request";
+import { playerbyteam } from "../../utils/API_urls";
+import { useTranslation } from "react-i18next";
+import clublogo from "../../assets/images/club-logo2.jpg";
 
 function PlayersBanner() {
-  const [data, setData] = useState(null)
-  const { id } = useParams()
-  const { t, i18n } = useTranslation()
-  const lng = i18n.language
+  const [data, setData] = useState(null);
+  const { id } = useParams();
+  const { t, i18n } = useTranslation();
+  const lng = i18n.language;
   useEffect(() => {
     getRequest(playerbyteam + id)
-      .then(response => {
-        setData(response?.data)
+      .then((response) => {
+        setData(response?.data);
+        console.log(response?.data);
       })
-      .catch(error => {
-        console.log(error)
-      })
-  }, [id])
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -30,36 +31,36 @@ function PlayersBanner() {
   }, []);
 
   const name =
-    lng === "uz"
-      ? data?.name_uz
-      : lng === "ru"
-        ? data?.name_ru
-        : data?.name_en;
+    lng === "uz" ? data?.name_uz : lng === "ru" ? data?.name_ru : data?.name_en;
   const coachname =
     lng === "uz"
       ? data?.coach?.name_uz
       : lng === "ru"
-        ? data?.coach?.name_ru
-        : data?.coach?.name_en;
+      ? data?.coach?.name_ru
+      : data?.coach?.name_en;
 
   return (
-    <div className={styles.banner} data-aos="fade-up"> {/* Yangi animatsiya */}
-      <div className='container'>
+    <div className={styles.banner} data-aos="fade-up">
+      {" "}
+      {/* Yangi animatsiya */}
+      <div className="container">
         <div className={styles.banner_part}>
-          <div className={styles.left} data-aos="fade-up"> {/* Yangi animatsiya */}
+          <div className={styles.left} data-aos="fade-up">
+            {" "}
+            {/* Yangi animatsiya */}
             <img src={data?.icon ?? clublogo} alt="club logosi" />
             <div className={styles.text_part}>
               <span>{name}</span>
-              <span>{data?.players?.count} {t("players")} </span>
+              <span>
+                {data?.players?.count} {t("players")}{" "}
+              </span>
               <span>{coachname}</span>
             </div>
           </div>
-          <div className={styles.button_part} data-aos="fade-up"> {/* Yangi animatsiya */}
-            <Link to="/" style={{ textDecoration: "none" }}>
-              {/* <button>
-                View team profile <img src={navigateicon} alt="navigate iconkasi" />
-              </button> */}
-            </Link>
+          <div className={styles.button_part} data-aos="fade-up">
+            {" "}
+            {/* Yangi animatsiya */}
+            {data?.banner && <img src={data?.banner} alt="" />}
           </div>
         </div>
       </div>
